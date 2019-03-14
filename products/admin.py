3a99f77cbecb13f7ddaab1970a4742a36e88fd77
kodevyas <arvindjha683@gmail.com>
2019-03-14 18:24:06 +0000

    product/admin: Add ProductAdmin with all the features

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('sku', 'name', 'active')
+    list_editable = ('active', )
+    list_filter = ('active', )
+    search_fields = ('sku', 'name')
