b7ccb722778e06d300204f86d9ed4905d66a40f1
kodevyas <arvindjha683@gmail.com>
2019-03-14 19:04:21 +0000

    pages/urls: Move upload page to /

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UploadView
 
 urlpatterns = [
-        path("upload", UploadView.as_view(), name='upload'),
+        path('', UploadView.as_view(), name='upload'),
         ]
